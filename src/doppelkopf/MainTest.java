6b0e60bba3862787a8f39582e968c2cd045912ce
Nghiem Phan <nghiemphan93@gmail.com>
2018-06-02 19:38:36 +0200

    delete some stuff

--- src/doppelkopf/MainTest.java
+++ src/doppelkopf/MainTest.java
@@ -7,21 +7,6 @@ import doppelkopf.Model.CardModel.*;
 import java.util.*;
 
 public class MainTest {
-//    public static ArrayList<String> init(){
-//        Suit suits[] = Suit.values();
-//        Rank ranks[] = Rank.values();
-//        ArrayList<String> cards = new ArrayList<>();
-//
-//        for(Suit suit : suits){
-//            for(Rank rank : ranks){
-//                cards.add(suit + " " + rank);
-//                cards.add(suit + " " + rank);
-//            }
-//        }
-//
-//    return cards;
-//    }
-
     public static <T> void display(ArrayList<T> list){
         Iterator<T> iterator = list.iterator();
         while (iterator.hasNext()){
@@ -29,38 +14,6 @@ public class MainTest {
         }
         System.out.println();
     }
-
-
-
-//    public static ArrayList<String> filterFehl(ArrayList<String> list){
-//        ArrayList<String> resultList = new ArrayList<>();
-//
-//        Iterator<String> iterator = list.iterator();
-//        while (iterator.hasNext()){
-//            String temp = iterator.next();
-//            switch (temp){
-//                case "PIK ASS":
-//                case "PIK KOENIG":
-//                case "PIK ZEHN":
-//                case "KREUZ ASS":
-//                case "KREUZ KOENIG":
-//                case "KREUZ ZEHN":
-//                case "HERZ ASS":
-//                case "HERZ KOENIG": resultList.add(temp);
-//            }
-//        }
-//
-//        return resultList;
-//    }
-
-//    public static ArrayList<String> filterTrumpf(ArrayList<String> list){
-//        ArrayList<String> resultList = list;
-//        ArrayList<String> fehl = filterFehl(list);
-//
-//        resultList.removeAll(fehl);
-//
-//        return resultList;
-//    }
 
     public static void testDeck(){
         CardsToDeal cardsToDeal = new CardsToDeal(null);
@@ -73,26 +26,12 @@ public class MainTest {
             }else{
                 System.out.println(card + " TRUMPF " + card.display() + " "+ card.getStrength() + " strong " + card.getPoint() + " points");
             }
-
         }
     }
 
     public static void main(String[] args) {
-
-       // GameController gameController = new GameController();
-       // gameController.initGame();
-       // gameController.startGame();
         Doppelkopf game = new GameController();
         game.play();
-
-
-//        for(int i = 0; i<50; i++){
-//            int playerWannaGuess = ThreadLocalRandom.current().nextInt(0, 5);
-//            System.out.println(playerWannaGuess);
-//        }
-
-
-
 
     }
 }
